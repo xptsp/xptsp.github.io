905c5128404ef93ddb3e800077992c6ca0e180d8
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2024-12-04 11:40:09 -0600

    Updated category and tag layouts

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733332255',resources: [
+    cacheName: 'chirpy-1733333760',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
