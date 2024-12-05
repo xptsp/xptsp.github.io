106ebb5b7226ea1cbc0cb00ea8e7e12ff6abb5a6
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2024-12-04 18:37:54 -0600

    Updated XPtsp GUI post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733351117',resources: [
+    cacheName: 'chirpy-1733358950',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
