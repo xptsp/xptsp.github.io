4b402f3ed54bb9a8285a35e8913497ef8a7ab24f
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2024-12-06 06:09:08 -0600

    Added DSI Changelog post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733449330',resources: [
+    cacheName: 'chirpy-1733486888',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
