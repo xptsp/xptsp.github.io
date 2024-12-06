da036291e8c8968ea85dd8cacf74beacdb768522
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2024-12-05 19:43:01 -0600

    Pinned Topics On Top in Category and Tags layouts

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733433329',resources: [
+    cacheName: 'chirpy-1733449330',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
